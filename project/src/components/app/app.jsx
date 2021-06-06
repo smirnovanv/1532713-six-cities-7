1e38666752a387dac760cb9b6049b3108736cd01
smirnovanv <smirnovanv89@gmail.com>
2021-06-06 14:46:29 +0300

    Add rendering cards

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Main from '../main/main';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {apartments} = props;
+  return (
+    <Main apartments={apartments}/>
+  );
 }
+
+App.propTypes = {
+  apartments: PropTypes.array.isRequired,
+};
 
 export default App;
